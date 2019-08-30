9b52ad8a0ddf2e2027ac8cb6b3e8d54f66b44e0d
hyhcloud <h_yhcloud@163.com>
2019-08-30 12:09:47 +0800

    hello

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <h1>hello</h1>
+    <h1>helloworld</h1>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
